f11b9bf1b8f7a13d81f9db03c4c0186ac03db425
Franco Marini <franco.marini@radiumrocket.com>
2022-10-29 17:45:43 -0300

    IS: Setup seeders example commented

---
+++ seeders/config.ts
@@ -0,0 +1,14 @@
+export default {
+  remove: true,
+  create: true,
+  // -- FIREBASE --
+  firebaseUsers: {
+    remove: true,
+    create: true,
+  },
+  // -- MONGO --
+  users: {
+    remove: true,
+    create: true,
+  },
+};
